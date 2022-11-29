d89738be9ba03a848fbc5361bc82aded9e49c7bf
KuzyaBaka <youryz58@gmail.com>
2022-11-29 15:24:08 +0300

    edit ex 4 to app js

--- js/app.js
+++ js/app.js
@@ -29,10 +29,39 @@ let res2 = +num % 2 === 0 ? `чётное` : `нечётное`;
 let res3 = +num % 1 === 0 ? `недробное` : `дробное`;
 let res4 = +num > 0 ? `больше нуля` : `меньше нуля`;
 
-alert(`Ваше число ${res1}, ${res2}, ${res3}, ${res4}`)
+alert(`Ваше число ${res1}, ${res2}, ${res3}, ${res4}`);
 
 // Задание 3.1
 
 const numOne = prompt(`Введите первое число`);
 const numTwo = prompt(`Введите второе число`);
 const numThree = prompt(`Введите третье число`);
+
+// Задание 4
+
+const num1 = +prompt(`Введите число от нуля до девяти`);
+
+switch (num1) {
+  case 0:
+    alert(`Нуль`)
+  case 1:
+    alert(`Один`);
+  case 2:
+    alert(`Два`);
+  case 3:
+    alert(`Три`);
+  case 4:
+    alert(`Четыре`);
+  case 5:
+    alert(`Пять`);
+  case 6:
+    alert(`Шесть`);
+  case 7:
+    alert(`Семь`);
+  case 8:
+    alert(`Восемь`);
+  case 9:
+    alert(`Девять`);
+  default:
+    alert(`Введите корректное число`)
+}
